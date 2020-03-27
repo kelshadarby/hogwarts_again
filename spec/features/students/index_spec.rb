63889c21b3d05513dec2dcd09f0720010bd464b6
Kelsha_Darby <ohanaipo@gmail.com>
2020-03-27 10:57:11 -0600

    Add too index spec to test for student name order

--- spec/features/students/index_spec.rb
+++ spec/features/students/index_spec.rb
@@ -35,4 +35,42 @@ RSpec.describe "as a visitor", type: :feature do
       expect(page).to_not have_content(longbottom.house)
     end
   end
+  it "can see all students in alphabetical order" do
+    snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+    hagarid = Professor.create(name: "Rubus Hagarid", age: 38 , specialty: "Care of Magical Creatures")
+    lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+
+    harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+    malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+    longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+
+    ProfessorStudent.create(student_id: harry.id, professor_id: snape.id)
+    ProfessorStudent.create(student_id: harry.id, professor_id: hagarid.id)
+    ProfessorStudent.create(student_id: harry.id, professor_id: lupin.id)
+    ProfessorStudent.create(student_id: malfoy.id, professor_id: hagarid.id)
+    ProfessorStudent.create(student_id: malfoy.id, professor_id: lupin.id)
+    ProfessorStudent.create(student_id: longbottom.id, professor_id: snape.id)
+
+    visit "/students"
+
+    within "#student-#{harry.id}" do
+      expect(page).to have_content("#{harry.name}: 3")
+      expect(page).to_not have_content(harry.age)
+      expect(page).to_not have_content(harry.house)
+    end
+    within "#student-#{malfoy.id}" do
+      expect(page).to have_content("#{malfoy.name}: 2")
+      expect(page).to_not have_content(malfoy.age)
+      expect(page).to_not have_content(malfoy.house)
+    end
+    within "#student-#{longbottom.id}" do
+      expect(page).to have_content("#{longbottom.name}: 1")
+      expect(page).to_not have_content(longbottom.age)
+      expect(page).to_not have_content(longbottom.house)
+    end
+
+    expect(page.all('.individual-student')[0]).to have_content(malfoy.name)
+    expect(page.all('.individual-student')[1]).to have_content(harry.name)
+    expect(page.all('.individual-student')[2]).to have_content(longbottom.name)
+  end
 end
