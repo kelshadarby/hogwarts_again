85819cd9b1c35cd0380cc9229d4b9257a76af8f3
Kelsha_Darby <ohanaipo@gmail.com>
2020-03-27 09:54:04 -0600

    Fix typos in professors show spec

--- spec/features/professors/show_spec.rb
+++ spec/features/professors/show_spec.rb
@@ -3,28 +3,25 @@ require 'rails_helper'
 RSpec.describe "as a visitor", type: :feature do
   it "can visit the professors index page and see al professors with their name, age and specialty" do
     snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
-    lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
 
     harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
-    malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
     longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
 
     ProfessorStudent.create(student: harry, professor: snape)
     ProfessorStudent.create(student: longbottom, professor: snape)
-    ProfessorStudent.create(student: malfoy, professor: lupin)
 
     visit "/professors/#{snape.id}"
 
     within '.student-names-list' do
       within "#student-#{harry.id}" do
-        expect(page).to have_content(snape.name)
-        expect(page).to_not have_content(snape.age)
-        expect(page).to_not have_content(snape.house)
+        expect(page).to have_content(harry.name)
+        expect(page).to_not have_content(harry.age)
+        expect(page).to_not have_content(harry.house)
       end
       within "#student-#{longbottom.id}" do
-        expect(page).to have_content(longobttom.name)
-        expect(page).to_not have_content(longobttom.age)
-        expect(page).to_not have_content(longobttom.house)
+        expect(page).to have_content(longbottom.name)
+        expect(page).to_not have_content(longbottom.age)
+        expect(page).to_not have_content(longbottom.house)
       end
     end
   end
